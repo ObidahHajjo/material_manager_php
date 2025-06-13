ec7a6c6bd9b51c21d63cde70285df9239b993cd0
ObidahHajjo <hajjoobidah@gmail.com>
2025-06-14 01:37:11 +0200

    fix : delete dump and fpdf

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,18 +6,7 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit7fe2a9c13f6b95a40d635825e0dd2176
 {
-    public static $files = array (
-        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
-        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
-    );
-
     public static $prefixLengthsPsr4 = array (
-        'S' => 
-        array (
-            'Symfony\\Polyfill\\Mbstring\\' => 26,
-            'Symfony\\Component\\VarDumper\\' => 28,
-        ),
         'R' => 
         array (
             'Routes\\' => 7,
@@ -37,14 +26,6 @@ class ComposerStaticInit7fe2a9c13f6b95a40d635825e0dd2176
     );
 
     public static $prefixDirsPsr4 = array (
-        'Symfony\\Polyfill\\Mbstring\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
-        ),
-        'Symfony\\Component\\VarDumper\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/var-dumper',
-        ),
         'Routes\\' => 
         array (
             0 => __DIR__ . '/../..' . '/Routes',
@@ -65,7 +46,6 @@ class ComposerStaticInit7fe2a9c13f6b95a40d635825e0dd2176
 
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
-        'FPDF' => __DIR__ . '/..' . '/setasign/fpdf/fpdf.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
